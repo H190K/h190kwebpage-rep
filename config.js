33292996d986c3365c691ab805ddbc59ed5752a1
Hasan Albehadili <72431824+H190K@users.noreply.github.com>
2025-02-14 18:14:02 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+    emailjs: {
+        publicKey: "49_dulv6CMLuR4L-c",
+        serviceID: "service_41ua8qp",
+        templateID: "template_v3r1opc"
+    },
+    payments: {
+        nowPaymentsApiKey: "J0QACAH-BTH4F4F-QDXM4ZS-RCA58BH"
+    }
+};
+
+export default config;
