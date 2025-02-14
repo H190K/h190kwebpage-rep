48e9b3921e34e4693976f79fb400abd53787a017
Hasan Albehadili <72431824+H190K@users.noreply.github.com>
2025-02-14 18:30:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 const config = {
     emailjs: {
-        publicKey: "49_dulv6CMLuR4L-c",
+        publicKey: "49_dulv6CMLuR4L-c", // Ideally, move this to environment variables
         serviceID: "service_41ua8qp",
         templateID: "template_v3r1opc"
-    },
-    payments: {
-        nowPaymentsApiKey: "J0QACAH-BTH4F4F-QDXM4ZS-RCA58BH"
     }
 };
 
